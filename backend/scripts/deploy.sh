b9dba0edefa131f15c033fcfccb9592f77c0d574
Danish-ali <0000danishsh@gmail.com>
2026-08-06 00:27:11 +0500

    fixed small problem

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
 AWS_REGION=${{secrets.AWS_REGION}}
-BACKEND_REGISTORY=${{secrets.BACKEND_ECR_REGISTORY}}
-IMAGE=${{FRONTEND_ECR_REGISTORY}}:latest
+BACKEND_REGISTORY=${{secrets.BACKEND_REGISTORY}}
+IMAGE=${{BACKEND_REGISTORY}}:latest
 
 aws ecr login --region $AWS_REGION | docker login --username AWS --pasword-sdtin $BACKEND_REGISTORY
-aws ecr login --region $AWS_REGION | docker login --username AWS --pasword-stdin $FRONTEND_REGISTORY
+aws ecr login --region $AWS_REGION | docker login --username AWS --pasword-stdin $BACKEND_REGISTORY
  docker pull $IMAGE
- if [$(docker ps -a -q -f name=frontend-container) ]; then
- docker stop frontend-container
- docker rm frontend-container
+ if [$(docker ps -a -q -f name=backend-container) ]; then
+ docker stop backend-container
+ docker rm backend-container
  fi
 
  aws secretsmanager --get-secret-value --secret-id "prod/db-secrets" --region $AWS_REGION --query  SecretString  --output text | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' > .env
 
- docker  run -d  -p 5000:5000 --name frontend-container --env-file .env  --restart on-failure $IMAGE
+ docker  run -d  -p 5000:5000 --name backend-container --env-file .env  --restart on-failure $IMAGE
 
  docker prune -f
