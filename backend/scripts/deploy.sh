d74d95718b46a40b20c66a648ea58d6340db9685
Danish-ali <0000danishsh@gmail.com>
2026-08-06 00:14:35 +0500

    fixed appspec.yml

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
 AWS_REGION=${{secrets.AWS_REGION}}
-FRONTEND_REGISTORY=${{secrets.FRONTEND_ECR_REGISTORY}}
+BACKEND_REGISTORY=${{secrets.BACKEND_ECR_REGISTORY}}
 IMAGE=${{FRONTEND_ECR_REGISTORY}}:latest
 
+aws ecr login --region $AWS_REGION | docker login --username AWS --pasword-sdtin $BACKEND_REGISTORY
 aws ecr login --region $AWS_REGION | docker login --username AWS --pasword-stdin $FRONTEND_REGISTORY
  docker pull $IMAGE
- if [$(docker ps -a -q -f name=frontend-container) ]; then 
+ if [$(docker ps -a -q -f name=frontend-container) ]; then
  docker stop frontend-container
- docker rm frontend-container 
+ docker rm frontend-container
  fi
 
  aws secretsmanager --get-secret-value --secret-id "prod/db-secrets" --region $AWS_REGION --query  SecretString  --output text | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' > .env
- 
- docker  run -d  -p 80:80 --name frontend-container --env-file .env  --restart on-failure $IMAGE
 
- docker prune -f 
-  
+ docker  run -d  -p 5000:5000 --name frontend-container --env-file .env  --restart on-failure $IMAGE
+
+ docker prune -f
