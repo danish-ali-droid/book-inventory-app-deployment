75efb48058139d4b1891be3dd2101496a3244184
Danish-ali <0000danishsh@gmail.com>
2026-08-06 21:09:15 +0500

    fixed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,7 +18,7 @@ aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --
  docker rm frontend-container 
  fi
 
- aws secretsmanager --get-secret-value --secret-id "prod/db-secrets" --region $AWS_REGION --query  SecretString  --output text | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' > .env
+ aws secretsmanager get-secret-value --secret-id "prod/db-secrets" --region $AWS_REGION --query  SecretString  --output text | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' > .env
  
  docker  run -d  -p 80:80 --name frontend-container --env-file .env  --restart on-failure $IMAGE
 
